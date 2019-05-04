5644d6fd37cf2b80bdffac1e8ee94ec928da62c2
angelinayhl <huilan.you@qq.com>
2019-05-04 16:52:07 -0400

    "Submit HW3 - Angelina You"

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -15,19 +15,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
+
+import codecs
+import bibtexparser
 import re
 import os
 import glob
-import requests
+from collections import Counter
 from types import SimpleNamespace
 import tldextract
-from collections import Counter
-from collections import defaultdict
 
-
-from types import SimpleNamespace
-
-# Task1
+# Task 1
 
 def load_map(map_file):
     """
@@ -42,22 +40,6 @@ def load_map(map_file):
         if len(l) == 3:
             key = l[0]
             d[key] = SimpleNamespace(weight=float(l[1]), series=l[2])
-
-    return d
-
-import bibtexparser
-
-
-# load the tsv file
-def load_email(map_file):
-    fin = open(map_file)
-    d = {}
-
-    for i, line in enumerate(fin):
-        l = line.strip().split('\t')
-        key = l[0]
-        d[key] = l[2:]
-
 
     return d
 
@@ -93,18 +75,12 @@ def get_entry_dict(bib_map, bib_dir):
 
     bibs = {}
     for k, v in bib_map.items():
-        fin = open(os.path.join(bib_dir, k+'.bib'))
+        fin = open(os.path.join(bib_dir, k+'.bib'), encoding='utf-8')
         bib = bibtexparser.loads(fin.read())
         bibs.update([get(entry, v.weight, v.series) for entry in bib.entries if valid(entry, v.weight)])
 
     return bibs
 
-MAP_FILE = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/dat/bib_map.tsv'
-BIB_DIR = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/bib/'
-
-bib_map = load_map(MAP_FILE)
-entry_dict = get_entry_dict(bib_map, BIB_DIR)
-print(len(entry_dict))
 
 def get_email_dict(txt_dir):
     """
@@ -125,22 +101,27 @@ def get_email_dict(txt_dir):
 
         return ' '.join(doc)
 
-    re_email = re.compile('[({\[]?\s*([a-z0-9\.\-_]+(?:\s*[,;|]\s*[a-z0-9\.\-_]+)*)\s*[\]})]?\s*@\s*([a-z0-9\.\-_]+\.[a-z]{2,})')
-    email_dict = {}
+    re_email = re.compile(
+        '[({\[]?\s*([a-z0-9\.\-_]+(?:\s*[,;|]\s*[a-z0-9\.\-_]+)*)\s*[\]})]?\s*@\s*([a-z0-9\.\-_]+\.[a-z]{2,})')
+    email_dict_new = {}
 
     for txt_file in glob.glob(os.path.join(txt_dir, '*.txt')):
         try:
             doc = chunk(txt_file)
         except UnicodeDecodeError:
-            # print(txt_file)
             continue
         emails = []
 
-        pattern1 = ['initial.last1', 'initial.last2']
+        pattern1 = ['lastname.firstname']
+        pattern2 = ['initial.last', 'initial.surname']
+        pattern3 = ['surname','lastname']
 
         for m in re_email.findall(doc):
+
             ids = m[0].replace(';', ',').replace('|', ',')
             domain = m[1]
+
+            ## Consider different patterns of emails
 
             if ids in pattern1:
                 name1 = []
@@ -148,9 +129,9 @@ def get_email_dict(txt_dir):
                 if key in entry_dict:
 
                     for k in entry_dict[key]['author']:
-                        initial = k.split(' ')[0][1].lower()
+                        first = k.split(' ')[0].lower()
                         last = k.split(' ')[1].lower()
-                        name1.append(initial + '.' + last)
+                        name1.append(last + '.' + first)
 
                     ids = ','.join(name1)
 
@@ -160,21 +141,70 @@ def get_email_dict(txt_dir):
                             continue
                         emails.append(ID.strip() + '@' + domain)
 
+
+
                 break
 
-            if ',' in ids:
-                emails.extend([ID.strip()+'@'+domain for ID in ids.split(',') if ID.strip()])
+            elif ids in pattern2:
+                name2 = []
+                key = os.path.basename(txt_file)[:-4]
+                if key in entry_dict:
+
+                    for k in entry_dict[key]['author']:
+                        initial = k.split(' ')[0][1].lower()
+                        last = k.split(' ')[1].lower()
+                        name2.append(initial + '.' + last)
+
+                    ids = ','.join(name2)
+
+                if ',' in ids:
+                    for ID in ids.split(','):
+                        if ID.strip() in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                            continue
+                        emails.append(ID.strip() + '@' + domain)
+
+
+
+                break
+
+            elif ids in pattern3:
+                name3 = []
+                key = os.path.basename(txt_file)[:-4]
+                if key in entry_dict:
+
+                    for k in entry_dict[key]['author']:
+                        last = k.split(' ')[1].lower()
+                        name3.append(last)
+
+                    ids = ','.join(name3)
+
+                if ',' in ids:
+                    for ID in ids.split(','):
+                        if ID.strip() in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                            continue
+                        emails.append(ID.strip() + '@' + domain)
+
+
+
+                break
+
             else:
-                emails.append(ids+'@'+domain)
+                if ',' in ids:
+                    for ID in ids.split(','):
+                        if ID.strip() in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                            continue
+                        emails.append(ID.strip() + '@' + domain)
+                else:
+                    emails.append(ids + '@' + domain)
 
         if emails:
             key = os.path.basename(txt_file)[:-4]
-            email_dict[key] = emails
+            email_dict_new[key] = emails
 
-TXT_DIR = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/txt/'
-email_dict = get_email_dict(TXT_DIR)
 
-def print_emails(entry_dict, email_dict, email_file):
+    return email_dict_new
+
+def print_emails(entry_dict, email_dict_new, email_file, weights_dict):
     """
     :param entry_dict: the output of get_entry_dict().
     :param email_dict: the output of get_email_dict().
@@ -186,45 +216,91 @@ def print_emails(entry_dict, email_dict, email_file):
     for k, v in sorted(entry_dict.items()):
         n = len(v['author'])
         l = [k, str(n)]
-        if k in missing: l.extend(missing[k][:n])
-        if k in email_dict and k not in missing: l.extend(email_dict[k][:n])
+        if k in email_dict_new:
+            emails = [';'.join(email_dict_new[k][:n])]
+            if k in weights_dict:
+                l = [k, str(n), str(emails), str(weights_dict[k])]
+
         fout.write('\t'.join(l) + '\n')
-
-file = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/dat/email_map.tsv'
-
-email_map = load_email(file)
-
-EMAIL_FILE = 'email_map.tsv'
-print_emails(entry_dict, email_dict, file)
+    fout.close()
 
 
+def print_mismatch(entry_dict, email_dict_new, email_mismatch):
+
+    fout = open(email_mismatch, 'w')
+
+    for k, v in sorted(entry_dict.items()):
+        n = len(v['author'])
+
+        if k in email_dict_new:
+            e_count = len(email_dict_new[k])
+            if e_count != n:
+                l = [k, str(n), str(e_count), str(email_dict_new[k])]
+                fout.write('\t'.join(l) + '\n')
 
 
-# Task2
+    fout.close()
 
-def weight(email_dict):
-    inst = {}
 
-    for ID, v in email_dict.items():
-        inst[ID] = []
-        for email in v:
-            identifier = email.rfind('@')
-            domain = email[identifier + 1:]
-            split = domain.split('.')
+# Task 2
 
-            if len(split) <= 2:
-                institution = split[0].strip()
-            else:
-                institution = split[-2].strip()
+def get_domains(email_dict_new):
 
-            inst[ID].insert(0, institution)
+    domain = {}
 
-        total = len(inst[ID])
-        inst[ID] = Counter(inst[ID])
+    for k, v in email_dict_new.items():
+        domains = []
+        for z in email_dict_new[k]:
+            domains.append(str(z.split('@')[1]))
 
-        for weight in inst[ID]:
-            inst[ID][weight] = round(inst[ID][weight] / total, 2)
+        domain[k] = domains
 
-    inst[ID] = sorted(inst[ID].items())
+    return domain
 
-    return (inst)
+def get_weights(domain):
+    weights = {}
+
+    for k, v in domain.items():
+        num_dom = len(v)
+        dom = []
+        w = Counter([t for t in v])
+        y = len(Counter([t for t in v]))
+        doms = {}
+
+        if y >= 1:
+            for k1, v1 in w.items():
+                ext = tldextract.extract(k1)
+                main = '.'.join(ext[1:3])
+                doms[main] = doms.get(main, 0) + v1
+
+
+            for k2, v2 in doms.items():
+                #                 print(k2)
+                weigh = int(v2) / int(num_dom)
+                weigh = round(weigh, 2)
+                dom.append((' ' + str(k2) + ":" + str(weigh)).strip())
+
+            weights[k] = ';'.join(dom)
+
+    return weights
+
+if __name__ == '__main__':
+    BIB_DIR ='/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/bib/'
+    MAP_FILE = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/dat/bib_map.tsv'
+
+    bib_map = load_map(MAP_FILE)
+    entry_dict = get_entry_dict(bib_map, BIB_DIR)
+
+    TXT_DIR = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-m'
+    email_dict_new = get_email_dict(TXT_DIR)
+    print(email_dict_new)
+
+    EMAIL_MISMATCH = 'you_mismatch.tsv'
+    print_mismatch(entry_dict, email_dict_new, EMAIL_MISMATCH)
+
+    # Task 2: Institution Weighting
+    domains_dict = get_domains(email_dict_new)
+    weights_dict = get_weights(domains_dict)
+
+    EMAIL_FILE = '/Users/huilanyou/PycharmProjects/qtm385/hw3/nlp-ranking-master/dat/email_map.tsv'
+    print_emails(entry_dict, email_dict_new, EMAIL_FILE, weights_dict)
